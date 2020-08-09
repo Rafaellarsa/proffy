f3005846fad2f339a9e6c197b9fba4d84d67897f
Rafaellarsa <46501003+Rafaellarsa@users.noreply.github.com>
2020-08-09 00:58:40 -0300

    Proffys info turned dymanic

--- src/server.js
+++ src/server.js
@@ -11,12 +11,12 @@ const proffys = [
         time_to: [1220]
     },
     {
-        name: "Daniele Evangelista",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        name: "Marcelo d'Almeida",
+        avatar: "https://scontent.fnat1-1.fna.fbcdn.net/v/t31.0-8/886821_1019851494756720_486201231599763764_o.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=GNRP8LDlQegAX-TNiqj&_nc_ht=scontent.fnat1-1.fna&oh=f6377a0896646da401b55e01597e6787&oe=5F54E48C",
         whatsapp: "899876543534",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
+        bio: "Amo machine learning e ver a Nami lambendo o próprio bucho.<br><br>Infelizmente não vou poder te dar aula porque tô ocupado sendo CTO do Nubank.",
+        subject: "Nada",
+        cost: "999999",
         weekday: [0],
         time_from: [720],
         time_to: [1220]
@@ -28,7 +28,7 @@ function pageLanding(req, res) {
 }
 
 function pageStudy(req, res) {
-    return res.render("study.html")
+    return res.render("study.html", { proffys })
 }
 
 function pageGiveClasses(req, res) {
