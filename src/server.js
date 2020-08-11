4ad4f39aff801c12af90a57f4a93170252281ffa
Rafaellarsa <46501003+Rafaellarsa@users.noreply.github.com>
2020-08-11 15:02:59 -0300

    Added favicons and improved data submission and filter

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 //yarn run dev
 
-//servidor
 const express = require("express")
 const server = express()
 
-const { pageLanding, pageStudy, pageGiveClasses } = require("./pages")
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require("./pages")
 
 //configurar nunjucks
 const nunjucks = require("nunjucks")
@@ -15,11 +14,14 @@ nunjucks.configure("src/views", {
 
 //Início e configuração do servidor
 server
+//Receber os dados do req.body
+.use(express.urlencoded({ extended: true}))
 //Configuração dos arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
 //Rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 //Start do servidor
-.listen(5500)
+.listen(3000)
